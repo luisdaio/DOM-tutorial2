8dbf6fbd7124061c11a5a56a1f038c7698336a12
Luís Fernandes <luisdaiofernandes@gmail.com>
2023-10-03 23:34:03 +0100

    Added key press event listener in order to play sounds.

--- index.js
+++ index.js
@@ -3,38 +3,46 @@ document.querySelectorAll("button").forEach(b => {
     b.addEventListener("click", function () {
 
         b.setAttribute("class", "pressed");
-        
-        switch (b.innerText) {
-            case "w":
-                var audio1 = new Audio("sounds/tom-1.mp3");
-                audio1.play();
-                break;
-            case "a":
-                var audio2 = new Audio("sounds/tom-2.mp3");
-                audio2.play();
-                break;
-            case "s":
-                var audio3 = new Audio("sounds/tom-3.mp3");
-                audio3.play();
-                break;
-            case "d":
-                var audio4 = new Audio("sounds/tom-4.mp3");
-                audio4.play();
-                break;
-            case "j":
-                var audio5 = new Audio("sounds/snare.mp3");
-                audio5.play();
-                break;
-            case "k":
-                var audio6 = new Audio("sounds/crash.mp3");
-                audio6.play();
-                break;
-            case "l":
-                var audio7 = new Audio("sounds/kick-bass.mp3");
-                audio7.play();
-                break;
-            default:
-                break;
-        }
+
+        playSoundByLetter(b.innerText);
     });
 });
+
+document.addEventListener("keypress", function(event){    
+    playSoundByLetter(event.key);
+});
+
+function playSoundByLetter(letter){
+    switch (letter) {
+        case "w":
+            var audio1 = new Audio("sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio("sounds/tom-2.mp3");
+            audio2.play();
+            break;
+            case "s":
+            var audio3 = new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+            case "d":
+                var audio4 = new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+            case "j":
+            var audio5 = new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio("sounds/crash.mp3");
+            audio6.play();
+            break;
+            case "l":
+            var audio7 = new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break;
+        default:
+            break;
+    }
+}
