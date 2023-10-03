c0042e17a06bd4e1116c80a946191f86da1d00ea
Luís Fernandes <luisdaiofernandes@gmail.com>
2023-10-03 23:21:19 +0100

    Correct switch case statement.

--- index.js
+++ index.js
@@ -3,34 +3,35 @@ document.querySelectorAll("button").forEach(b => {
     b.addEventListener("click", function () {
 
         b.setAttribute("class", "pressed");
+        
         switch (b.innerText) {
             case "w":
-                var audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
+                var audio1 = new Audio("sounds/tom-1.mp3");
+                audio1.play();
                 break;
             case "a":
-                var audio = new Audio("sounds/tom-2.mp3");
-                audio.play();
+                var audio2 = new Audio("sounds/tom-2.mp3");
+                audio2.play();
                 break;
             case "s":
-                var audio = new Audio("sounds/tom-3.mp3");
-                audio.play();
+                var audio3 = new Audio("sounds/tom-3.mp3");
+                audio3.play();
                 break;
             case "d":
-                var audio = new Audio("sounds/tom-4.mp3");
-                audio.play();
+                var audio4 = new Audio("sounds/tom-4.mp3");
+                audio4.play();
                 break;
             case "j":
-                var audio = new Audio("sounds/snare.mp3");
-                audio.play();
+                var audio5 = new Audio("sounds/snare.mp3");
+                audio5.play();
                 break;
             case "k":
-                var audio = new Audio("sounds/crash.mp3");
-                audio.play();
+                var audio6 = new Audio("sounds/crash.mp3");
+                audio6.play();
                 break;
             case "l":
-                var audio = new Audio("sounds/kick-bass.mp3");
-                audio.play();
+                var audio7 = new Audio("sounds/kick-bass.mp3");
+                audio7.play();
                 break;
             default:
                 break;
